5fccb5b7811ea9bf6fa268703166fe694b4ccea3
Victor Soto <jvictorsotom@gmail.com>
2017-06-01 16:19:29 +0200

    feat(helpers): Implemented helper methods to check iface and exec cmds

---
+++ src/helpers.js
@@ -0,0 +1,29 @@
+import os from 'os';
+import { exec } from 'child-process-promise';
+
+const debug = require('debug')('tc-wrapper');
+
+// Does a network iface exists?
+function checkNetworkIface(iface) {
+  return os.networkInterfaces()[iface] !== undefined;
+}
+
+// Helper function to exec commands.
+function execCmd(cmd, allowedErrors = []) {
+  debug(`About to execute cmd: ${cmd} with allowed errors: ${JSON.stringify(allowedErrors)}`);
+  return exec(cmd)
+    .then((result) => {
+      debug(`Executed successfully cmd: ${cmd}: `, result.stdout);
+      const { stdout } = result;
+      return stdout;
+    })
+    .catch((error) => {
+      debug(`Executed with error cmd: ${cmd}: `, error.stdout, error.stderr);
+      if (allowedErrors.some(allowedError => allowedError.test(error.stderr))) {
+        return Promise.resolve(error.stderr);
+      }
+      return Promise.reject(error);
+    });
+}
+
+export default { checkNetworkIface, execCmd };
